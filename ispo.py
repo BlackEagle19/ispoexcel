3dab83f60bffc7e54d2f897dd702be4b373f6f15
BlackEagle19 <blackeagle.1903nf@gmail.com>
2024-03-06 17:59:11 +0300

    Changes to be committed: 	modified:   ispo.py

--- ispo.py
+++ ispo.py
@@ -1,10 +1,5 @@
 import pandas as pd
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import requests
 from bs4 import BeautifulSoup
 
