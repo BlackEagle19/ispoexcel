d53ed25a4bdb0d28eeecb23a282abebdefd5ea63
BlackEagle19 <blackeagle.1903nf@gmail.com>
2024-03-06 19:05:14 +0300

    Changes to be committed: 	modified:   tel.py

--- tel.py
+++ tel.py
@@ -1,19 +1,15 @@
 from telegram import Bot
 from telegram import InputFile
 import telegram.ext
-import logging
+import asyncio
 
-def send_excel_file(excel_file_path, bot_token, chat_id):
+async def send_excel_file(excel_file_path, bot_token, chat_id):
     # Bot'u başlat
     bot = Bot(token=bot_token)
 
-    # Loglama yapısını ayarla (isteğe bağlı)
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-
     # Excel dosyasını gönder
     with open(excel_file_path, 'rb') as file:
-        bot.send_document(chat_id=chat_id, document=file)
+        await bot.send_document(chat_id=chat_id, document=file)
 
 # Bot token'ınızı ve hedef chat ID'nizi girin
 BOT_TOKEN = '6994284905:AAHhU7PleaVU3eSlMFt_qt3imu_KWHmya0c'
@@ -23,4 +19,4 @@ CHAT_ID = '-4157001586'  # Kanal için "@channelusername" şeklinde de kullanıl
 EXCEL_FILE_PATH = 'IspoBilgileri.xlsx'
 
 # Fonksiyonu çağır
-send_excel_file(EXCEL_FILE_PATH, BOT_TOKEN, CHAT_ID)
+asyncio.run(send_excel_file(EXCEL_FILE_PATH, BOT_TOKEN, CHAT_ID))
